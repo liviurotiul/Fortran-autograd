f2836c23ad2ab0a9722540796cceb5b8845e4b27
liviurotiul <liviurotiultelefon@gmail.com>
2020-05-11 00:10:07 +0300

    added type for list of pointers to block types and generic interface for pointer function

--- autograd.f95
+++ autograd.f95
@@ -1,14 +1,31 @@
 module Autograd
 	implicit none
 
+
+	! i need this type to store the children 
+	type, public :: block_ptr
+		type(Block), pointer :: p
+	end type block_ptr
+
+	abstract interface
+		subroutine generic_back_subr(this_ptr, other_ptr)
+			type(Block), pointer :: this_ptr, other_ptr
+		end subroutine
+	end interface
+
 	type, public :: Block
 		real :: data, grad
+		type(block_ptr), dimension(:), allocatable :: children
+		procedure(generic_back_subr), pointer :: backward_
 		contains
 			procedure, pass(this) :: construct_ => constructor_definition !in place constructor
 			procedure, pass(this) :: add => addition_definition
+			! procedure, pass(this) :: backward_ => backward_definition
 	end type Block
 
+
 	contains
+
 		function construct(data) result(output)
 			type(Block) :: output
 			real :: data
@@ -24,9 +41,16 @@ module Autograd
 		end subroutine constructor_definition
 
 		function addition_definition(this, other) result(output)
-			class(Block), intent(in) :: this, other
-			type(Block) :: output
+			type(Block), intent(in) :: this
+			type(Block) :: output, other
 			output = construct(this%data + other%data)
+			! contains
+			! 	subroutine backward_()
+			! 		this%grad = this%grad + output%grad
+			! 		other%grad = other%grad + output%grad
+			! 	end subroutine
+			! output%backward_ => backward_
+
 		end function addition_definition
 
 end module Autograd
