cf8617b172f9a27e08f7199680d679b848c9d73d
liviurotiul <liviurotiultelefon@gmail.com>
2020-05-10 17:55:25 +0300

    created normal constructor

--- autograd.f95
+++ autograd.f95
@@ -4,15 +4,21 @@ module Autograd
 	type, public :: Block
 		real :: data, grad
 		contains
-			procedure, pass(this) :: construct => constructor_definition
+			procedure, pass(this) :: construct_ => constructor_definition !in place constructor
 			procedure, pass(this) :: add => addition_definition
 	end type Block
 
 	contains
+		function construct(data) result(output)
+			type(Block) :: output
+			real :: data
+			output%data = data
+			output%grad = 0
+		end function construct
 
 		subroutine constructor_definition(this, data)
 			class(Block) :: this
-			real :: data, other
+			real :: data
 			this%data = data
 			this%grad = 0
 		end subroutine constructor_definition
@@ -20,7 +26,7 @@ module Autograd
 		function addition_definition(this, other) result(output)
 			class(Block), intent(in) :: this, other
 			type(Block) :: output
-			call output%construct(this%data + other%data)
+			output = construct(this%data + other%data)
 		end function addition_definition
 
 end module Autograd
@@ -31,9 +37,8 @@ program main
 	use Autograd
 	implicit none
 	type(Block) :: A, B, C
-	call A%construct(3.0)
-	call B%construct(4.0)
+	A = construct(30.0)
+	B = construct(40.0)
 	C = A%add(B)
-	C = C%add(B)
 	print *, C%data
 end program main
