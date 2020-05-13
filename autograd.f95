ecf37083fc9646baac5177608f17706852908839
liviurotiul <liviurotiultelefon@gmail.com>
2020-05-13 16:04:50 +0300

    fixed stuff and added comments

--- autograd.f95
+++ autograd.f95
@@ -21,16 +21,17 @@ module Autograd
 
 end module Autograd
 
-module  FTL
+module  FTL !we need a queue for the differentiation grpah
 	use Autograd
 	implicit none
+	! the Q contains references to objects taking part in certain operations
 	type, public :: reference
 		type(Block), pointer :: this_ptr, other_ptr
 		character(len = 20) :: operation
 	end type reference
 
 	type, public :: queue
-		real, dimension(:), allocatable :: list
+		type(reference), dimension(:), allocatable :: list
 		contains
 			procedure, pass(this) :: append => append_definition
 			procedure, pass(this) :: print => print_definition
@@ -49,8 +50,8 @@ module  FTL
 
 		subroutine append_definition(this, item)
 			class(queue) :: this
-			real, allocatable, dimension(:) :: new
-			real :: item
+			type(reference), allocatable, dimension(:) :: new
+			type(reference) :: item
 			if (.not. allocated(this%list)) then
 				allocate(this%list(0))
 			end if
@@ -64,59 +65,19 @@ module  FTL
 
 		subroutine print_definition(this)
 			class(queue) :: this
-			print *, this%list
+			print *, "this%list"
 		end subroutine print_definition
 end module FTL
-
-
-
-
-
-! module Autograd
-! 	implicit none
-! 	type, public :: Block
-! 		real :: data, grad
-! 		contains
-! 			procedure, pass(this) :: construct_ => constructor_definition !in place constructor
-! 			procedure, pass(this) :: add => addition_definition
-! 	end type Block
-
-
-! 	contains
-
-! 		function construct(data) result(output)
-! 			type(Block) :: output
-! 			real :: data
-! 			output%data = data
-! 			output%grad = 0
-! 		end function construct
-
-! 		subroutine constructor_definition(this, data)
-! 			class(Block) :: this
-! 			real :: data
-! 			this%data = data
-! 			this%grad = 0
-! 		end subroutine constructor_definition
-
-! 		function addition_definition(this, other) result(output)
-! 			class(Block), intent(in) :: this
-! 			type(Block) :: output, other
-! 			output = construct(this%data + other%data)
-! 		end function addition_definition
-
-! end module Autograd
-
-
 
 program main
 	use FTL
 	use Autograd
 	implicit none
-	type(queue) :: q
-	type(Block) :: a
-	call q%append(3.0)
-	call q%append(4.0)
-	call q%print()
+	! type(queue) :: q
+	! type(Block) :: a
+	! call q%append(3.0)
+	! call q%append(4.0)
+	! call q%print()
 	! call q%print
 	! type(Block) :: A, B, C
 	! A = construct(30.0)
